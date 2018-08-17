46487b705c3afcf4b59b1f5f454d432323a56611
unknown <chalubinska.paulina@gmail.com>
2018-08-17 18:47:54 +0200

    improve hamburger animation

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 var btn = document.querySelector('.hamburger');
-var menu = document.querySelector('.main-nav')
+var menu = document.querySelector('.main-nav');
 
 btn.addEventListener('click', () => {
   console.log("clicked");
   menu.classList.toggle('main-nav');
+  btn.classList.toggle('open');
 })
