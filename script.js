ececbfd274d8ab0c791abaa36659d610ef7c1dc3
unknown <chalubinska.paulina@gmail.com>
2018-08-24 11:35:31 +0200

    add toggle menu to every menu item selected

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
-var btn = document.querySelector('.hamburger');
-var menu = document.querySelector('.main-nav');
+const btn = document.querySelector('.hamburger');
+const menu = document.querySelector('.main-nav');
+const menuItems = document.querySelectorAll('.nav__menu li');
 
 btn.addEventListener('click', () => {
-  console.log("clicked");
   menu.classList.toggle('main-nav__open');
   btn.classList.toggle('open');
-})
+});
+for (let i=0; i< menuItems.length; i++) {
+  menuItems[i].addEventListener('click', (menuItem) => {
+    menu.classList.toggle('main-nav__open');
+    btn.classList.toggle('open');
+  });
+}
